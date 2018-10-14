83f80318119403870e0ce88233174a57053c1dc3
irwinmck <irwinmck@gmail.com>
2018-10-14 12:37:56 -0400

    Add ability to load more info page about specific models.

--- finalProject/v2/app.js
+++ finalProject/v2/app.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 // test schema setup, essentially a model for a database object
 var modelSchema = new mongoose.Schema({
   title: String,
-  image: String
+  image: String,
+  description: String
 });
 
 // assigning that schema to a var we can use to create objects
@@ -23,7 +24,8 @@ var Model3d = mongoose.model("Model", modelSchema);
 // Model3d.create(
 //   {
 //     title: "Model 1",
-//     image: "https://www.lowpolylab.net/wp-content/uploads/edd/2017/02/low-poly-mill-1180x738.jpg" 
+//     image: "https://www.lowpolylab.net/wp-content/uploads/edd/2017/02/low-poly-mill-1180x738.jpg",
+//     description: "I can't remember what this image is so this is a filler description." 
 //   }, function(error, model3d) {
 //     if(error) console.log(error);
 //     else {
@@ -56,7 +58,8 @@ app.get("/models", function(req,res) {
 app.post("/models", function(req,res) {
   var title = req.body.title;
   var image = req.body.image;
-  var newModel = {title: title, image: image};
+  var description = req.body.description;
+  var newModel = {title: title, image: image, description: description};
   Model3d.create(newModel, function(error, new3dModel) {
     if(error) console.log(error);
     else {
@@ -69,6 +72,16 @@ app.post("/models", function(req,res) {
 // localhost:3000/models/new
 app.get("/models/new", function(req,res) {
   res.render("newModel.ejs");
+});
+
+// route that will show more info about a specific model
+app.get("/models/:id", function(req,res) {
+  Model3d.findById(req.params.id, function(error, foundModel) {
+    if(error) console.log(error);
+    else {
+      res.render("show.ejs", {model: foundModel});
+    }
+  });
 });
 
 // listen function for the app to determine where to host the server
